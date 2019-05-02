db98b78309dd86d7338fcac4cbf65861347cfd5d
Yerassyl <erasyl.boy@gmail.com>
2019-05-02 18:24:15 +0600

    deploy

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends React.Component {
 
   onButtonClick = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3001/imageurl', {
+    fetch('https://glacial-temple-44388.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends React.Component {
     .then(response => response.json())
     .then(response => {
       if(response){
-        fetch('http://localhost:3001/image', {
+        fetch('https://glacial-temple-44388.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
