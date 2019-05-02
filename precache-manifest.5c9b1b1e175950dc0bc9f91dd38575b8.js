afdd4ed8e1f0cea37b581c41eaa93fc696011923
GhoulIsReal <erasyl.boy@gmail.com>
2019-05-02 18:37:42 +0600

    Updates

---
+++ precache-manifest.5c9b1b1e175950dc0bc9f91dd38575b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d368b397ae773d64838f6703bdb8b072",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/face-detection/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ebec792fcd4be514474f",
+    "url": "/face-detection/static/css/main.c71dc121.chunk.css"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/face-detection/static/js/2.33497142.chunk.js"
+  },
+  {
+    "revision": "ebec792fcd4be514474f",
+    "url": "/face-detection/static/js/main.b8f2d253.chunk.js"
+  },
+  {
+    "revision": "47eac57ab0cf42a50c81",
+    "url": "/face-detection/static/js/runtime~main.21987f21.js"
+  }
+]);
